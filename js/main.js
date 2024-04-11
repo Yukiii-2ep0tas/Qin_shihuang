e8ed7ddbadaac0746c300f5565adb71d3e17eef8
Yukiii-2ep0tas <76561110+Yukiii-2ep0tas@users.noreply.github.com>
2024-04-11 23:05:40 +0800

    tianyang's change

--- js/main.js
+++ js/main.js
@@ -65,7 +65,7 @@
 	      controlNav: true,
 	      directionNav: false,
 	      smoothHeight: true,
-	      slideshowSpeed: 7000,
+	      slideshowSpeed: 999999,
 	      animationSpeed: 600,
 	      randomize: false,
 	   });
